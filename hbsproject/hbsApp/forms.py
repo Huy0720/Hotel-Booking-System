8bd468812378615ce1bc8781d7bf817e0e91c9eb
joshidevanshi <devanshi_joshi@mymail.sutd.edu.sg>
2022-07-05 11:30:32 +0800

    linked to database

--- hbsproject/hbsApp/forms.py
+++ hbsproject/hbsApp/forms.py
@@ -8,6 +8,9 @@ from django.forms import ModelForm
 class DateInput(forms.DateInput):
     input_type = 'date'
 
+Country_choices = [('destination search','Destination Search'),('india','India'),('china','China'),
+('hong kong','Hong Kong'), 
+('vietnam', 'Vietnam'),('new zealand', 'New Zealand'),]
 
 class SearchDestinationForm(forms.ModelForm):
     class Meta:
@@ -15,8 +18,11 @@ class SearchDestinationForm(forms.ModelForm):
         fields = ["country","people","checkin_date", "checkout_date"]
         widgets = {
             'checkin_date' : DateInput(),
-            'checkout_date' : DateInput()
+            'checkout_date' : DateInput(),
+            'country': forms.Select(choices=Country_choices),
         }
+        
+    
 
     def clean(self):
         cleaned_data = super().clean()
--- hbsproject/hbsApp/models.py
+++ hbsproject/hbsApp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class SearchCountry(models.Model):
-    countryname = models.CharField(max_length=250)
+#class SearchCountry(models.Model):
+    #countryname = models.CharField(max_length=250)
 
 class SearchDestination(models.Model):
     country = models.CharField(max_length=250)

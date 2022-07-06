b30f074bcd32fdf08bf4329f46d9b826bb736265
joshidevanshi <devanshi_joshi@mymail.sutd.edu.sg>
2022-07-07 01:00:18 +0800

    front end upload

--- hbsproject/hbsApp/urls.py
+++ hbsproject/hbsApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import *
+from . import views
 
 app_nmae = "hbsApp"
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    
+    #path('',views.saveitem),
 
 ]

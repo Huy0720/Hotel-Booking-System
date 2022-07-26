7ac3d75c1c51c43505fcd0293731493c5b417246
Huy Nguyen <76168837+Huy0720@users.noreply.github.com>
2022-07-26 22:50:53 +0800

    Update urls.py

--- hbsproject/hbsproject/urls.py
+++ hbsproject/hbsproject/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from hbsApp import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("hbsApp.urls")),
